71e0273f80b0d0ff447f7557b1302ff1b25016c0
bartolkaruza <bartol.k@gmail.com>
2023-10-22 12:12:58 +0000

    fix: remove notebook folder path saving

--- src/routes/user/notebook/notebook.ts
+++ src/routes/user/notebook/notebook.ts
@@ -4,20 +4,19 @@ import { Request, Response } from 'express';
 export const handleCreateUserNotebook = async (req: Request, res: Response) => {
   try {
     const { user_id } = req;
-    const { host, port, serverToken, notebookFolderPath } = req.body;
+    const { host, port, serverToken } = req.body;
 
     const upsertQuery = `
         INSERT INTO jupyter_settings (id, user_id, host, port, token, notebooks_folder_path)
-        VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5)
+        VALUES (uuid_generate_v4(), $1, $2, $3, $4)
         ON CONFLICT (user_id)
         DO UPDATE SET
           host = $2,
           port = $3,
           token = $4,
-          notebooks_folder_path = $5
         RETURNING host, port, token, notebooks_folder_path
       `;
-    const upsertValues = [user_id, host, port, serverToken, notebookFolderPath];
+    const upsertValues = [user_id, host, port, serverToken];
 
     const upsertResult = await withDbClient(
       async (client) => await client.query(upsertQuery, upsertValues)
