bb56a2ccc19901fb9378496ae81b746e416d50aa
bartolkaruza <bartol.k@gmail.com>
2023-11-08 11:30:14 +0000

    fix: add gpt-4-1106-preview model

--- src/lib/pricing.ts
+++ src/lib/pricing.ts
@@ -3,6 +3,7 @@ import { ModelId } from '@App/types/model';
 const modelToPrice = {
   'gpt-3.5-turbo': [0.0018, 0.0022],
   'gpt-4': [0.036, 0.072],
+  'gpt-4-1106-preview': [0.012, 0.024],
 };
 
 export function centsToCredits(cents: number) {
